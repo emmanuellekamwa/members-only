d6b2742ba8e3c18bffe0bb39fe36832ca0d3be49
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-02-15 13:03:32 +0100

    Reset migration

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,27 @@
 class PostController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(params_filter)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render new
+    end
+  end
+
+  def index
+    @post = Post.all.order('created_at DESC')
+  end
+
+  private
+
+  def params_filter
+    params.require(:post).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
+  root 'post#index'
+  resources :post, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
