88bfb64f38a291cb28ef5b097047d4150f82f6d5
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-02-20 14:44:20 +0100

    Change post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
-  root 'post#index'
-  get "post/new" => "post#new"
+  root 'posts#index'
+  get "posts/new" => "posts#new"
   resources :posts, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
