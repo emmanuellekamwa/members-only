0fff884ad59860bc2d9f68a372e57fb46a83e32e
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-02-24 10:44:10 +0100

    Add new helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def check_user
+    output = ''
+    if user_signed_in?
+      output << "<small>You are signed in as</small><small> #{current_user.name} </small>"
+      output << (link_to 'New Post', new_post_path, class: 'btn btn-success')
+      output << (link_to 'Log out', destroy_user_session_path, method: :delete,
+                                                               class: 'btn btn-success')
+    else
+      output << (link_to 'Sign in', user_session_path, class: 'btn btn-success')
+      output << (link_to 'Sign Up', new_user_registration_path, class: 'btn btn-success')
+    end
+    output.html_safe
+  end
 end
